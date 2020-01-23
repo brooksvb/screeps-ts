9930868445376086decf334d5ba385b9a2669725
Brooks <mail@brooks.codes>
2020-01-23 12:05:30 -0500

    Replace semicolons with commas

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,10 +2,10 @@
 
 // memory extension samples
 interface CreepMemory {
-  role: string;
-  room: string;
-  working: boolean;
-  upgrading: boolean;
+  role: string,
+  room: string,
+  working: boolean,
+  upgrading: boolean
 }
 
 interface Memory {
